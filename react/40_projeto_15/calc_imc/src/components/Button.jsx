77019985fe7f7902f1b8176f5b020e0facb9e228
Pedro Henrique <pedrohal2503@gmail.com>
2025-01-21 22:12:51 -0300

    seção 40 - projeto 15 - calculadora IMc

--- react/40_projeto_15/calc_imc/src/components/Button.jsx
+++ react/40_projeto_15/calc_imc/src/components/Button.jsx
@@ -1,8 +1,12 @@
 import "./Button.css";
 import React from "react";
 
-const Button = ({ id, text }) => {
-  return <button id={id}>{text}</button>;
+const Button = ({ id, text, action }) => {
+  const handleAction = (e) => {
+    action(e);
+  };
+
+  return <button id={id} onClick={handleAction}>{text}</button>;
 };
 
 export default Button;
