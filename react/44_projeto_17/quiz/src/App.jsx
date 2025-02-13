d77a12496936496eeb4eb95178b40ae6f07c2672
Pedro Henrique <pedrohal2503@gmail.com>
2025-02-13 10:23:20 -0300

    estilizando a tela inicial

--- react/44_projeto_17/quiz/src/App.jsx
+++ react/44_projeto_17/quiz/src/App.jsx
@@ -5,7 +5,7 @@ import Welcome from "./components/Welcome";
 
 function App() {
   return (
-    <div className="classname">
+    <div className="App">
       <h1>Quiz de Programação</h1>
       <Welcome />
     </div>
