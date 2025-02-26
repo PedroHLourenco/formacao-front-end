909816ea0e21dd93b980485ddfe8b69d1e368135
Pedro Henrique <pedrohal2503@gmail.com>
2025-02-26 10:27:54 -0300

    iniciando projeto countdown

---
+++ react/45_projeto_18/countdown/src/App.jsx
@@ -0,0 +1,32 @@
+// componentes
+import Title from "./components/Title";
+import Counter from "./components/Counter";
+
+// hooks
+import useCountdown from "./hooks/useCountdown";
+
+// img
+import NewYear from "./assets/newyear.jpg";
+
+// css
+import "./App.css";
+
+function App() {
+  const [day, hour, minute, second] = useCountdown("Jan, 2026, 00:00:00");
+
+  return (
+    <div className="App" style={{ backgroundImage: `url(${NewYear})` }}>
+      <div className="container">
+        <Title title="Contagem Regressiva para 2026" />
+        <div className="countdown-container">
+          <Counter title="Dias" number={day} />
+          <Counter title="Horas" number={hour} />
+          <Counter title="Minutos" number={minute} />
+          <Counter title="Segundos" number={second} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
