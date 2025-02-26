4751eed5e37cd0b0966f62ebbfda185c24971c22
Pedro Henrique <pedrohal2503@gmail.com>
2025-02-26 11:13:22 -0300

    adicionando context e navegação entre páginas

--- react/45_projeto_18/countdown/src/routes/Home.jsx
+++ react/45_projeto_18/countdown/src/routes/Home.jsx
@@ -1,24 +1,61 @@
 import React from "react";
 
+// hooks
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+// context
+import { CountdownContext } from "../context/CountdownContext";
+
 // css
 import "./Home.css";
 
 const Home = () => {
+  const [title, setTitle] = useState();
+  const [date, setDate] = useState();
+  const [image, setImage] = useState();
+  const [color, setColor] = useState();
+
+  const { setEvent } = useContext(CountdownContext);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const eventObject = {
+      title,
+      date,
+      image,
+      color,
+    };
+
+    setEvent(eventObject);
+    navigate("/countdown");
+  };
+
   return (
     <div className="home">
       <h2>Monte a sua contagem regressiva!</h2>
-      <form className="countdown-form">
+      <form className="countdown-form" onSubmit={handleSubmit}>
         <label>
           <span>Título:</span>
           <input
             type="text"
             name="title"
             placeholder="Digite o título do evento"
+            onChange={(e) => setTitle(e.target.value)}
+            required
           />
         </label>
         <label>
           <span>Data do evento:</span>
-          <input type="date" name="date" />
+          <input
+            type="date"
+            name="date"
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
         </label>
         <label>
           <span>Imagem:</span>
@@ -26,11 +63,16 @@ const Home = () => {
             type="text"
             name="image"
             placeholder="Informe a URL da imagem"
+            onChange={(e) => setImage(e.target.value)}
           />
         </label>
         <label>
           <span>Cor do tema:</span>
-          <input type="color" name="color" />
+          <input
+            type="color"
+            name="color"
+            onChange={(e) => setColor(e.target.value)}
+          />
         </label>
         <input type="submit" value="Enviar" />
       </form>
