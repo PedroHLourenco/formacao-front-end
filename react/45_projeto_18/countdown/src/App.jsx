9b6f1fee02094b20bdae81ddba8e78d4d44bd21c
Pedro Henrique <pedrohal2503@gmail.com>
2025-02-26 11:44:10 -0300

    seção 45 - projeto 18 - countdown

--- react/45_projeto_18/countdown/src/App.jsx
+++ react/45_projeto_18/countdown/src/App.jsx
@@ -2,6 +2,10 @@
 import { Outlet } from "react-router-dom";
 
 // hooks
+import { useContext } from "react";
+
+// context
+import { CountdownContext } from "./context/CountdownContext";
 
 // img
 import NewYear from "./assets/newyear.jpg";
@@ -10,10 +14,21 @@ import NewYear from "./assets/newyear.jpg";
 import "./App.css";
 
 function App() {
-  
+  const { event } = useContext(CountdownContext);
+
+  let eventImage = null;
+
+  if (event) eventImage = event.image;
 
   return (
-    <div className="App" style={{ backgroundImage: `url(${NewYear})` }}>
+    <div
+      className="App"
+      style={
+        eventImage
+          ? { backgroundImage: `url(${eventImage})` }
+          : { backgroundImage: `url(${NewYear})` }
+      }
+    >
       <div className="container">
         <Outlet />
       </div>
