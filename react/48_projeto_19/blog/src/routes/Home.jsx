f325dfc03bef8b83d298155fd57e105ad8f06b56
Pedro Henrique <pedrohal2503@gmail.com>
2025-03-17 11:15:07 -0300

    seção 48 - projeto 19 - blog utilizando axios

---
+++ react/48_projeto_19/blog/src/routes/Home.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+// css
+import "./Home.css";
+
+// hooks
+import { useState, useEffect } from "react";
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+
+      const data = response.data;
+
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Home;
