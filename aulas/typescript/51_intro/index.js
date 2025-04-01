2d5ee15a9d029448d3ff948f0e153f669efe5d66
Pedro Henrique <pedrohal2503@gmail.com>
2025-04-01 11:19:35 -0300

    seção 51 - introdução TypeScript

---
+++ aulas/typescript/51_intro/index.js
@@ -0,0 +1,10 @@
+// aula 727 - primeiro programa em TS
+var firstName = "Pedro";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá, " + name);
+}
+greeting(firstName);
+// greeting(anotherName);
+// greeting(x);
---
+++ aulas/typescript/51_intro/index.ts
@@ -0,0 +1,12 @@
+// aula 727 - primeiro programa em TS
+const firstName = "Pedro";
+const anotherName = 1;
+const x = true;
+
+function greeting(name: string) {
+  console.log("Olá, " + name);
+}
+
+greeting(firstName);
+// greeting(anotherName);
+// greeting(x);
