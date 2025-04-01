33829da70332457768a223975fea43d63eb637c1
Pedro Henrique <pedrohal2503@gmail.com>
2025-04-01 11:29:54 -0300

    desafio 1

---
+++ aulas/typescript/desafios/desafio_1/index.js
@@ -0,0 +1,7 @@
+// desafio 1: criar uma função que aceita dois argumentos, somente do tipo numérico (number) e exibir a soma entre os dois;
+var num1 = 5;
+var num2 = 10;
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(sum(num1, num2));
---
+++ aulas/typescript/desafios/desafio_1/index.ts
@@ -0,0 +1,10 @@
+// desafio 1: criar uma função que aceita dois argumentos, somente do tipo numérico (number) e exibir a soma entre os dois;
+
+const num1 = 5;
+const num2 = 10;
+
+function sum(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(sum(num1, num2));
