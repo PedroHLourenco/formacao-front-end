bad73cf30c00888e61e87a06e79d99faaacf4642
Pedro Henrique <pedrohal2503@gmail.com>
2025-04-09 14:23:04 -0300

    seção 56 - projeto 23 - github finder

---
+++ aulas/typescript/56_projeto_23/github-finder/src/components/BackBtn.tsx
@@ -0,0 +1,19 @@
+// hooks
+import { useNavigate } from "react-router-dom";
+
+// css
+import classes from "./BackBtn.module.css";
+
+const BackBtn = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <button className={classes.back_btn} onClick={() => navigate(-1)}>
+        Voltar
+      </button>
+    </>
+  );
+};
+
+export default BackBtn;
